0a8e5f66c7bdaa77e73d4301b67601acee94d90b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-29 12:46:04 +0100

    code convetions only.

--- src/main/resources/META-INF/resources/npo-api/authentication.js
+++ src/main/resources/META-INF/resources/npo-api/authentication.js
@@ -6,102 +6,105 @@
  * @constructor
  */
 
-var NpoApiAuthentication = function( apiKey, apiSecret, apiVersion) {
-    this.api_version = apiVersion || 'v1';
-    this.apiKey = apiKey;
-    this.apiSecret = apiSecret;
-};
+const NpoApiAuthentication = (function( apiKey, apiSecret, apiVersion) {
+    const api_version = apiVersion || 'v1';
+    const api_key = apiKey;
+    const api_secret = apiSecret;
 
-NpoApiAuthentication.prototype = {
     // private methods
-    _getCredentials: function (headers, resourcePath, params,  sorted) {
-        var location = document.location;
+    function getCredentials(headers, resourcePath, params,  sorted) {
+        const location = document.location;
         origin = params['x-origin'] || (location.origin || (location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : ''))); //IE :(
-        var data = 'origin:' + origin;
+        let data = 'origin:' + origin;
         if (! headers[ 'x-npo-date' ]) {
             headers['x-npo-date'] = new Date().toUTCString();
         }
         data += ',x-npo-date:' + headers['x-npo-date'];
 
         if (resourcePath) {
-            data += ',uri:/' + this.api_version + '/api/';
+            data += ',uri:/' + api_version + '/api/';
             data += resourcePath.split('?')[0];
-            data += this._getParametersForEncode(resourcePath, params, sorted);
+            data += getParametersForEncode(resourcePath, params, sorted);
         }
+        return  CryptoJS.HmacSHA256(data, api_secret).toString(CryptoJS.enc.Base64);
+    }
 
-        var base64 = CryptoJS.HmacSHA256(data, this.apiSecret).toString(CryptoJS.enc.Base64);
-        return base64;
-    },
-
-    _getParametersForEncode: function (resourcePath, params, sorted) {
-        params = params || this._getParametersFromResourcePath(resourcePath);
-        var sortedParameters = sorted ? params : this._getSortedParameters(params);
-        var result = "";
+    function getParametersForEncode(resourcePath, params, sorted) {
+        params = params || getParametersFromResourcePath(resourcePath);
+        const sortedParameters = sorted ? params : getSortedParameters(params);
+        let result = "";
         sortedParameters.forEach(function(s) {
             result += ',' + s.key + ':' + s.value;
         });
         return result;
-    },
-    _getParametersFromResourcePath: function (resourcePath) {
-        var questionMark = resourcePath.indexOf('?');
-        var hash = {};
+    }
+    function getParametersFromResourcePath(resourcePath) {
+        const questionMark = resourcePath.indexOf('?');
+        const hash = {};
         if (questionMark >= 0) {
-            var params = resourcePath.substring(questionMark + 1).split('&');
-            var paramLength = params.length;
+            const params = resourcePath.substring(questionMark + 1).split('&');
+            const paramLength = params.length;
 
-            for (var i = 0; i < paramLength; i++) {
-                var param = params[i].split('=');
+            for (let i = 0; i < paramLength; i++) {
+                const param = params[i].split('=');
                 hash[param[0]] = param[1];
             }
         }
         return hash;
-    },
-    _getSortedParameters: function (params) {
-        var ordered = [];
+    }
+    function getSortedParameters (params) {
+        const ordered = [];
         Object.keys(params).sort().forEach(function(key) {
             ordered.push({ 'key': key, 'value': params[key]});
         });
         return ordered;
-    },
-
-    /**
-     * Given a map of params, sort them and convert it to a query string
-     *
-     * @param {Object} params A map of parameters
-     * @param {boolean} sorted wether the give params are sorted already (defaults to false)
-     */
-    getQueryParameters: function(params, sorted) {
-        var sortedParameters = this._getSortedParameters(params, sorted);
-        var result = "";
-        var sep = '?';
-        sortedParameters.forEach(function(s) {
-            result += sep + s.key + '=' + encodeURIComponent(s.value);
-            sep = '&';
-        });
-        return result;
-
-    },
-
-    /**
-     * Calculates and adss the authorization header to to given map of headers
-     * @param {Object} headers the current headers. May include 'x-origin'
-     * @param {String} resourcePath The resource path on the api which will be queried (without /v1/api)
-     * @param {Object} params (optional) the query parameters that to be added. If not given, they will be parsed from the resourcePath
-     * @param {boolean} sorted wether the give params are sorted already (defaults to false)
-     */
-    addAuthorizationHeader: function (headers, resourcePath, params, sorted) {
-        headers['Authorization'] = this.getAuthorizationHeader(headers, resourcePath, params);
-    },
-
-    /**
-     * Calculates the authorization header to to given map of headers
-     * @param {Object} headers the current headers. May include 'x-origin'
-     * @param {String} resourcePath The resource path on the api which will be queried (without /v1/api)
-     * @param {Object} params (optional) the query parameters that to be added. If not given, they will be parsed from the resourcePath
-     * @param {boolean} sorted wether the give params are sorted already (defaults to false)
-     */
-    getAuthorizationHeader: function (headers, resourcePath, params, sorted) {
-        var header =  'NPO ' + this.apiKey + ":" + this._getCredentials(headers, resourcePath, params, sorted);
-        return header;
     }
-};
+
+
+    return Object.freeze({
+        /**
+         * Given a map of params, sort them and convert it to a query string
+         *
+         * @param {Object} params A map of parameters
+         * @param {boolean} sorted wether the give params are sorted already (defaults to false)
+         */
+        getQueryParameters: function (params, sorted) {
+            const sortedParameters = getSortedParameters(params, sorted);
+            let result = "";
+            let sep = '?';
+            sortedParameters.forEach(function (s) {
+                result += sep + s.key + '=' + encodeURIComponent(s.value);
+                sep = '&';
+            });
+            return result;
+
+        },
+
+        /**
+         * Calculates and adds the authorization header to the given map of headers
+         * @param {Object} headers the current headers. May include 'x-origin'
+         * @param {String} resourcePath The resource path on the api which will be queried (without /v1/api)
+         * @param {Object} params (optional) the query parameters that to be added. If not given, they will be parsed from the resourcePath
+         * @param {boolean} sorted wether the give params are sorted already (defaults to false)
+         */
+        addAuthorizationHeader: function (headers, resourcePath, params, sorted) {
+            headers['Authorization'] = this.getAuthorizationHeader(headers, resourcePath, params);
+        },
+
+        /**
+         * Calculates the authorization header to given map of headers
+         * @param {Object} headers the current headers. May include 'x-origin'
+         * @param {String} resourcePath The resource path on the api which will be queried (without /v1/api)
+         * @param {Object} params (optional) the query parameters that to be added. If not given, they will be parsed from the resourcePath
+         * @param {boolean} sorted wether the give params are sorted already (defaults to false)
+         */
+        getAuthorizationHeader: function (headers, resourcePath, params, sorted) {
+            return 'NPO ' + api_key + ":" + getCredentials(headers, resourcePath, params, sorted);
+        }
+    });
+})();
+
+try {
+    exports.NpoApiAuthentication = NpoApiAuthentication;
+} catch (e) { /* ignored for unsupported browsers */}
+
